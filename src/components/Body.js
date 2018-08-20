5aee072cda640cd39b9a40b8cc5c8ccc2e0ba86a
Edilson Borges <edilsonfb@gmail.com>
2018-08-19 23:59:55 -0300

    bug fix

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,14 +2,14 @@ import React, { Component } from "react";
 
 class Body extends Component {
   handleSubmit = (e) => {
-    this.setState({ codigo: e.codigo });
+    console.log(e);
+    // this.setState({ codigo: e.codigo });
   }
   render() {
     return (
       <form onSubmit={this.handleSubmit} className="card  text-center">
         <div className="card-body">
           <input className="form-control form-control-lg" placeholder="Digite o código de rastreio" />
-          <input class="btn btn-primary" type="button">Enviar</input>
         </div>
       </form>
     );
--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -7,15 +7,15 @@ class Header extends Component {
         <a className="btn btn-secondary" href="/">
           Rastreamento de Objetos
         </a>
-        <div class="navbar-collapse collapse w-100 order-3 dual-collapse2">
-          <ul class="navbar-nav ml-auto">
-            <li class="nav-item">
-              <a className="btn btn-secondary" target="_blank" href="https://twitter.com/edilsonfborges" >
+        <div className="navbar-collapse collapse w-100 order-3 dual-collapse2">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <a className="btn btn-secondary" target="_blank" rel="noopener noreferrer" href="https://twitter.com/edilsonfborges" >
                 <i className="fab fa-twitter" /> Twitter 
               </a>
             </li>
-            <li class="nav-item">
-              <a className="btn btn-secondary" target="_blank" href="https://github.com/edilsonborges/react-correios" >
+            <li className="nav-item">
+              <a className="btn btn-secondary" target="_blank" rel="noopener noreferrer" href="https://github.com/edilsonborges/react-correios" >
                 <i className="fab fa-github" /> GitHub
               </a>
             </li>
