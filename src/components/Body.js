a36025b6c72d65bb1def44a3a4722b9169f57ec3
Edilson Borges <edilson.borges@agrodefesa.go.gov.br>
2018-08-22 15:06:31 -0300

    handling fetch api

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,34 +1,25 @@
 import React, { Component } from "react";
 
 class Body extends Component {
-    state = {
-      codigo: 'PS158730655BR',
-      // codigo: 'PS153948797BR'
-    }
+  state = {
+    codigo: 'PS158730655BR',
+    // codigo: 'PS153948797BR'
+  }
   handleSubmit(e) {
     e.preventDefault();
     console.log(this.state.codigo);
-    // let objHeader = new Headers({
-    // 'Access-Control-Allow-Origin':'http://localhost:3000',
-    // 'Content-Type': 'multipart/form-data',
-    // 'Access-Control-Allow-Credentials': 'true'
-    // });
-    fetch('https://correios.postmon.com.br/rastreamento/?objeto='+this.state.codigo
-    // , { method: 'GET',
-    //            headers:{
-    //             'Access-Control-Allow-Origin':'*',
-    //             'Content-Type': 'multipart/form-data',
-    //             'Access-Control-Allow-Credentials': 'true'
-    //           },
-    //           headers: objHeader,
-    //           mode: 'cors',
-    //           cache: 'default' }
-)
-      .then((v) => (console.log(v.body)));
+    let result = fetch('http://127.0.0.1:8181/track/' + this.state.codigo + '/json', { method: "GET" })
+      .then(function (response) {
+        if (response.ok) {
+          return response.json();
+        }
+      }).catch(function (err) {
+        console.log(err)
+      });
+      console.log(result);
   }
   handleChange(e) {
     e.preventDefault();
-    // console.log(e.target.name+': '+e.target.value);
     this.setState({ [e.target.name]: e.target.value });
   }
   render() {
@@ -36,8 +27,8 @@ class Body extends Component {
       <form onSubmit={this.handleSubmit.bind(this)} className="card text-center">
         <div className="card-body">
           <input name="codigo" className="form-control form-control-lg"
-          placeholder="Digite o código de rastreio"
-          onChange={this.handleChange.bind(this)} value={this.state.codigo} />
+            placeholder="Digite o código de rastreio"
+            onChange={this.handleChange.bind(this)} value={this.state.codigo} />
         </div>
       </form>
     );
