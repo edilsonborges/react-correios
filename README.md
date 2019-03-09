293a5846766e6393155016fca49d711d2bc65018
Edilson Borges <edilsonfb@gmail.com>
2019-03-09 03:24:38 -0300

    gh-pages

--- README.md
+++
@@ -1,8 +0,0 @@
-# react-correios
-Aplicação para gerenciar encomendas dos correios que consome dados vindo da [correios-rest-api](https://github.com/edilsonborges/correios-rest-api) 📦
-
-- git clone https://github.com/edilsonborges/react-correios.git
-- cd react-correios
-- npm i ou yarn
-- renomear o arquivo .env.example e colocar o [token criado no mapbox](https://www.mapbox.com/help/how-access-tokens-work/)
-- npm start ou yarn start
---
+++ precache-manifest.6806e9c18358605b3081f3ef4c16ff46.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "603111ee06c9d3ab02e6",
+    "url": "/static/js/main.f713b130.chunk.js"
+  },
+  {
+    "revision": "ef1ce18ded0e731dccc9",
+    "url": "/static/js/2.36fd9d04.chunk.js"
+  },
+  {
+    "revision": "603111ee06c9d3ab02e6",
+    "url": "/static/css/main.cca62404.chunk.css"
+  },
+  {
+    "revision": "692dcb86c7fc7d5c3bb550692a6602ba",
+    "url": "/index.html"
+  }
+];
